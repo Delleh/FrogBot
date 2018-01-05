7889d20d9d95fec9c4310fecc22cdae9b141a16c
ILiedAboutCake <iliedaboutthecake@gmail.com>
2018-01-04 21:54:48 -0500

    add playing status

--- pepobot/discord.py
+++ pepobot/discord.py
@@ -1,6 +1,8 @@
 print("IMPORT: %s" % __name__)
 
+import asyncio
 import discord
+
 from . import config
 from . import entropy
 from discord.ext import commands
@@ -12,6 +14,10 @@ async def on_ready():
     entropy.resetPool()
     print("HELLO: Logged in to Discord as '{0.user.name}' ({0.user.id})".format(bot))
     print("HELLO: To add this bot to a discord server, use https://discordapp.com/oauth2/authorize?client_id={0.user.id}&scope=bot&permissions=0".format(bot))
+
+    await asyncio.sleep(5)
+    print('{0} Setting game status as in as "{1}"'.format(__name__, config.cfg['bot']['playing']))
+    await bot.change_presence(game=discord.Game(name=config.cfg['bot']['gamestatus']))    
 
 def objectFactory(snowflake):
     return discord.Object(snowflake)
